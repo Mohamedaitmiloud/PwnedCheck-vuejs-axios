cdce7c90265621a75f42b0906e482082101374fd
Mohamedaitmiloud <mohammed.aitmiloud@gmail.com>
2018-11-28 01:16:54 +0100

    adding sweet alert

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,12 +11,35 @@ new Vue({
         checkEmail:function(){
             axios.get('https://haveibeenpwned.com/api/v2/breachedaccount/'+this.email)
                  .then(response=>{
-                     this.foundData=true;
-                     this.data=response.data;
-                     this.email='';
+                     if(response.data){
+                        swal({
+                            title: response.data.length +' BREACH DETECTED!',
+                            width: 600,
+                            padding: '3em',
+                            background: '#fff url(/images/trees.png)',
+                            backdrop: `
+                              rgba(0,0,0,1)
+                              url("https://media.giphy.com/media/Zsx8ZwmX3ajny/giphy.gif")
+                              center left
+                              no-repeat
+                            `
+                          })
+                        this.foundData=true;
+                        this.data=response.data;
+                        this.email='';
+                     }else{
+
+                     }
+
                  })
                  .catch(error=>{
-                     console.log('error',error);
+                    swal({
+                        position: 'center',
+                        type: 'success',
+                        title: 'You are safe !',
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
                  })
         }
     }
